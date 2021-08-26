7a08e63ea37486eeb342d00b1311dc9ff95ca47f
Felipe Menezes <felipemenezes.web@gmail.com>
2021-08-26 05:32:38 -0300

    Adicionando configuração de instalação automatizada do frontend.

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 git clone https://github.com/felipemenezesdm/insights-backend.git ./src/node
 git clone https://github.com/felipemenezesdm/insights.git ./src/vue
 
-cd ./src/node && npm install
+cd ./src/node && npm install && npm install --only=dev
+
+cd ../vue && npm install
 
 docker-compose up -d
+
+docker-compose logs -f
